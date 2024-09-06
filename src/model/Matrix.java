c8e6393ed0f40ce59ddb22a08e5380ec30504343
jyan53 <jyan53@students.cs.ubc.ca>
2024-09-06 13:15:53 -0700

    Added my project

---
+++ src/model/Matrix.java
@@ -0,0 +1,8 @@
+package model;
+
+import java.util.List;
+
+public class Matrix {
+    private List<Row> matrix;
+    
+}
---
+++ src/model/Row.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Row {
+    private List<Double> row;
+
+    public Row() {
+        row = new ArrayList<>();
+    }
+
+}
